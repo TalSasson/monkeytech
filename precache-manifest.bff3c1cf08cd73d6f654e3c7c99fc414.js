9556f826dcaaada402601cd8858587ac6abdc6cf
Tal Sasson <tal.sason1@gmail.com>
2020-02-08 20:53:07 +0200

    Updates

---
+++ precache-manifest.bff3c1cf08cd73d6f654e3c7c99fc414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b7133ee5f9d399705b441d3b368d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac59da90bf29c154ffb7",
+    "url": "/static/css/main.9a196862.chunk.css"
+  },
+  {
+    "revision": "16ad376f6894c9775db3",
+    "url": "/static/js/2.b5387338.chunk.js"
+  },
+  {
+    "revision": "ac59da90bf29c154ffb7",
+    "url": "/static/js/main.61ceb2cb.chunk.js"
+  },
+  {
+    "revision": "ab624d7d45f6c97fa807",
+    "url": "/static/js/runtime-main.ce64b112.js"
+  }
+]);
