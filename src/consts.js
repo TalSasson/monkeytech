21f2bd3e8b22c7cdb2229685b6d080c234e1a005
Tal Sasson <tal.sason1@gmail.com>
2020-02-08 21:37:38 +0200

    use https for api

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 const TOKEN = process.env.REACT_APP_TOKEN || '433898df4a3e992b8411004109e4d574a90695e39e'
-const FAST_RIDER_URL = 'http://fast-rider.herokuapp.com/api/v1'
+const FAST_RIDER_URL = 'https://fast-rider.herokuapp.com/api/v1'
 const ERROR_MSG = 'Sorry, unable to fetch data'
 const LOCAL_STORAGE_KEY = 'fastRider-pinCode'
 
